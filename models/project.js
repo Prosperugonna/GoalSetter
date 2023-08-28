e900e3742ba3de7acd24c37bafece35e5fdd08bc
Prosperugonna <prosperugonna@gmail.com>
2023-08-28 17:46:09 +0100

    project model

---
+++ models/project.js
@@ -0,0 +1,32 @@
+#!usr/bon/node
+"""Project  model"""
+const mongoose = require('mongoose');
+
+const projectSchema = new mongoose.Schema({
+    user_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User', // Reference to the 'User' model
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    description: String,
+    sent: {
+        type: Boolean,
+        default: false
+    },
+    expiry_date: {
+        type: Date,
+        default: Date.now() + 3 * 24 * 60 * 60 * 1000 // 3 days from now
+    },
+    is_priority: {
+        type: Boolean,
+        default: false
+    }
+}, { timestamps: true });
+
+const Project = mongoose.model('Project', projectSchema);
+
+module.exports = Project;
