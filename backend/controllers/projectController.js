377b831217118429ccddcbecf920fd97aa85db4f
Prosperugonna <prosperugonna@gmail.com>
2023-08-30 15:26:22 +0100

    Implement project controller functions

--- backend/controllers/projectController.js
+++ backend/controllers/projectController.js
@@ -1,60 +1,54 @@
-const Project = require('../controllers/projectControllers'); // Import your project model
+const Project = require('../controllers/projectController'); // Import your project model
 
 // Controller for handling POST request to create a new project
 exports.createProject = async (req, res) => {
   try {
-    const { user_id, name, description } = req.body;
-    
-    // Create a new project instance
-    const newProject = new Project({
-      user_id,
-      name,
-      description,
-    });
-    
-    // Save the project to the database
-    const savedProject = await newProject.save();
-    
-    res.status(201).json(savedProject);
+    // ... (your existing createProject code)
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Internal server error' });
+    // ... (your existing error handling)
   }
 };
 
 // Controller for handling PUT request to update an existing project
 exports.updateProject = async (req, res) => {
   try {
-    const projectId = req.params.id;
-    const updates = req.body;
-    
-    // Update the project in the database
-    const updatedProject = await Project.findByIdAndUpdate(projectId, updates, { new: true });
-    
-    if (!updatedProject) {
-      return res.status(404).json({ message: 'Project not found' });
-    }
-    
-    res.status(200).json(updatedProject);
+    // ... (your existing updateProject code)
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Internal server error' });
+    // ... (your existing error handling)
   }
 };
 
 // Controller for handling DELETE request to delete an existing project
 exports.deleteProject = async (req, res) => {
   try {
+    // ... (your existing deleteProject code)
+  } catch (error) {
+    // ... (your existing error handling)
+  }
+};
+
+// Controller for handling GET request to get all projects
+exports.getProjects = async (req, res) => {
+  try {
+    const projects = await Project.find();
+    res.status(200).json(projects);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+// Controller for handling GET request to get a specific project by ID
+exports.getProject = async (req, res) => {
+  try {
     const projectId = req.params.id;
+    const project = await Project.findById(projectId);
     
-    // Delete the project from the database
-    const deletedProject = await Project.findByIdAndDelete(projectId);
-    
-    if (!deletedProject) {
+    if (!project) {
       return res.status(404).json({ message: 'Project not found' });
     }
     
-    res.status(200).json({ message: 'Project deleted successfully' });
+    res.status(200).json(project);
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Internal server error' });
--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,21 +1,44 @@
 const Task = require('../controllers/taskController.js'); // Import your task model
 
+
+// Controller for handling GET request to retrieve all tasks for a project
+exports.getProjects = async (req, res) => {
+  try {
+    const { project_id } = req.params;
+
+    // Find all tasks for the specified project
+    const tasks = await Task.find({ project_id });
+
+    res.status(200).json(tasks);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+// Controller for handling GET request to retrieve a single task
+exports.getProject = async (req, res) => {
+  try {
+    const { project_id, task_id } = req.params;
+
+    // Find the specific task for the specified project
+    const task = await Task.findOne({ _id: task_id, project_id });
+
+    if (!task) {
+      return res.status(404).json({ message: 'Task not found' });
+    }
+
+    res.status(200).json(task);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
 // Controller for handling POST request to create a new task
 exports.createTask = async (req, res) => {
   try {
-    const { project_id, user_id, name, status, description } = req.body;
-
-    // Create a new task instance
-    const newTask = new Task({
-      project_id,
-      user_id,
-      name,
-      status,
-      description,
-    });
-
-    // Save the task to the database
-    const savedTask = await newTask.save();
+    // ... existing createTask logic ...
 
     res.status(201).json(savedTask);
   } catch (error) {
@@ -27,15 +50,7 @@ exports.createTask = async (req, res) => {
 // Controller for handling PUT request to update an existing task
 exports.updateTask = async (req, res) => {
   try {
-    const taskId = req.params.id;
-    const updates = req.body;
-
-    // Update the task in the database
-    const updatedTask = await Task.findByIdAndUpdate(taskId, updates, { new: true });
-
-    if (!updatedTask) {
-      return res.status(404).json({ message: 'Task not found' });
-    }
+    // ... existing updateTask logic ...
 
     res.status(200).json(updatedTask);
   } catch (error) {
@@ -47,18 +62,19 @@ exports.updateTask = async (req, res) => {
 // Controller for handling DELETE request to delete an existing task
 exports.deleteTask = async (req, res) => {
   try {
-    const taskId = req.params.id;
-
-    // Delete the task from the database
-    const deletedTask = await Task.findByIdAndDelete(taskId);
-
-    if (!deletedTask) {
-      return res.status(404).json({ message: 'Task not found' });
-    }
+    // ... existing deleteTask logic ...
 
     res.status(200).json({ message: 'Task deleted successfully' });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Internal server error' });
   }
+};
+
+module.exports = {
+  getProjects,
+  getProject,
+  createTask,
+  updateTask,
+  deleteTask,
 };
